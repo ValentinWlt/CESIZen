2c3ab82949a8dfbd9ca5f273ee0cace1d44a9e36
Valentin <valentinwaultier@gmail.com>
2025-07-21 22:18:26 +0200

    retour version 8.014

--- CESIZen.Tests/obj/Debug/net8.0/CESIZen.Tests.AssemblyInfo.cs
+++ CESIZen.Tests/obj/Debug/net8.0/CESIZen.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CESIZen.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bafa22d81aeea4e22cd1b248affa213274b75cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e78505119b961509bf0f7976c62d44639add1bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("CESIZen.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CESIZen.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
