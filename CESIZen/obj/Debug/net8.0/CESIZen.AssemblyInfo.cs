2d1201b9b1a8fb38d08daadfbc85c208316c5363
Valentin <valentinwaultier@gmail.com>
2025-07-21 22:13:22 +0200

    retour en arrière maj packages

--- CESIZen/obj/Debug/net8.0/CESIZen.AssemblyInfo.cs
+++ CESIZen/obj/Debug/net8.0/CESIZen.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CESIZen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c81583e6bae946080c2ea9dbad469e1c598be616")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bafa22d81aeea4e22cd1b248affa213274b75cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("CESIZen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CESIZen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
