23416b1a77e8db8bd202fe25b48fd4e4a8007ac1
Valentin <valentinwaultier@gmail.com>
2025-07-25 11:55:37 +0200

    fix

--- CESIZen/Controllers/HomeController.cs
+++ CESIZen/Controllers/HomeController.cs
@@ -13,35 +13,10 @@ public class HomeController : Controller
         _logger = logger;
     }
 
-        public IActionResult Index()
-        {
-            // ❌ ERREURS VOLONTAIRES POUR SONARCLOUD (à ajouter dans votre méthode existante)
-            
-            // Erreur 1: Variable non utilisée (Code Smell)
-            var unusedVariable = "Cette variable ne sert à rien";
-            
-            // Erreur 2: Division par zéro (Bug Critique)
-            var testNumber = 42;
-            var zero = 0;
-            
-            try 
-            {
-                var result = testNumber / zero; // Division par zéro - SonarCloud va détecter ça !
-                ViewBag.TestResult = $"Résultat: {result}";
-            }
-            catch (Exception ex)
-            {
-                ViewBag.TestResult = $"Erreur capturée: {ex.Message}";
-            }
-            
-            // Erreur 3: Code jamais atteint (Code Smell)
-            if (false)
-            {
-                Console.WriteLine("Ce code ne s'exécutera jamais");
-            }
-
-            return View();
-        }
+    public IActionResult Index()
+    {
+        return View();
+    }
 
     public IActionResult Privacy()
     {
